453a3667007e2589fc02282d7c27c5f9e3a24aa5
Veres Bogdan <veresbogdan91@gmail.com>
2014-05-27 20:43:24 +0300

    Initial commit

---
+++ src/com/mmday/MMD/MainActivity.java
@@ -0,0 +1,62 @@
+
+package com.mmday.MMD;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.mmday.MMD.arc.ArcMenu;
+import com.mmday.MMD.arc.RayMenu;
+
+public class MainActivity extends Activity {
+	private static final int[] ITEM_DRAWABLES = { R.drawable.composer_camera, R.drawable.composer_music,
+			R.drawable.composer_place, R.drawable.composer_sleep, R.drawable.composer_thought, R.drawable.composer_with };
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		ArcMenu arcMenu = (ArcMenu) findViewById(R.id.arc_menu);
+        ArcMenu arcMenu2 = (ArcMenu) findViewById(R.id.arc_menu_2);
+
+        initArcMenu(arcMenu, ITEM_DRAWABLES);
+        initArcMenu(arcMenu2, ITEM_DRAWABLES);
+
+		RayMenu rayMenu = (RayMenu) findViewById(R.id.ray_menu);
+        final int itemCount = ITEM_DRAWABLES.length;
+		for (int i = 0; i < itemCount; i++) {
+			ImageView item = new ImageView(this);
+			item.setImageResource(ITEM_DRAWABLES[i]);
+
+			final int position = i;
+			rayMenu.addItem(item, new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
+				}
+			});// Add a menu item
+		}
+	}
+
+    private void initArcMenu(ArcMenu menu, int[] itemDrawables) {
+        final int itemCount = itemDrawables.length;
+        for (int i = 0; i < itemCount; i++) {
+            ImageView item = new ImageView(this);
+            item.setImageResource(itemDrawables[i]);
+
+            final int position = i;
+            menu.addItem(item, new OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
